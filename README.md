196a87d8cfda42ed7ce7ad7bd2cf90b15148f0b9
Matthew Lanham <matt@swanify.com>
2014-06-12 22:02:50 +0100

    ...

--- README.md
+++ README.md
@@ -9,8 +9,20 @@ All of the browser dependencies have been loaded via bower, to install them simp
 bower install
 ```
 
+## Node dependencies
+There is a simple node server included, you must first load the dependencies
+```
+npm install
+```
+
 ## Serving the project
 To serve the project, simply run:
 ```
 node app.js
+```
+
+## Viewing the application
+Once the node server is running, you can visit the following url to see the application
+```
+http://localhost:3000
 ```
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express       = require('express')
     ,app          = express()
     ,cors         = require('cors')
     ,path         = require('path')
-    ,logfmt       = require("logfmt")
+    ,logfmt       = require('logfmt')
     ,bodyParser   = require('body-parser')
     ,cookieParser = require('cookie-parser')
     ,fs           = require('fs')
@@ -13,12 +13,11 @@ app.configure(function() {
     app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
     app.use(express.logger('dev'));                     // log every request to the console
     app.use(bodyParser());                              // pull information from html in POST
-    app.use(express.methodOverride());                  // simulate DELETE and PUT
 });
 
 
 // Now listen for connections
 var port = Number(process.env.PORT || 3000);
 app.listen(port, function() {
-    console.log("Listening on " + port);
+    console.log("Listening, visit http://localhost:" + port);
 });
